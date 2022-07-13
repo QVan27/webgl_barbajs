570cadc5045f60ac6e5ac390de107ee7e83bff4d
QVan27 <qvannarath@gmail.com>
2022-07-14 00:06:30 +0200

    ✅ Effect with corners

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,7 @@ import fragment from "../../shaders/fragment.glsl";
 import vertex from "../../shaders/vertex.glsl";
 import testTexture from "../../assets/img/texture.jpg";
 import * as dat from "dat.gui";
+import gsap from "gsap";
 
 const cameraDistance = 600;
 
@@ -73,6 +74,7 @@ export default class Sketch {
         uProgress: { value: 1.0 },
         uTexture: { value: new THREE.TextureLoader().load(testTexture) },
         uTextureSize: { value: new THREE.Vector2(100, 100) },
+        uCorners: { value: new THREE.Vector4(0, 0, 0, 0) },
         uResolution: { value: new THREE.Vector2(this.width, this.height) },
         uQuadSize: { value: new THREE.Vector2(300, 300) },
       },
@@ -80,17 +82,48 @@ export default class Sketch {
       fragmentShader: fragment,
     });
 
+    this.tl = gsap
+      .timeline()
+      .to(this.material.uniforms.uCorners.value, {
+        x: 1,
+        duration: 1,
+      })
+      .to(
+        this.material.uniforms.uCorners.value,
+        {
+          y: 1,
+          duration: 1,
+        },
+        0.1
+      )
+      .to(
+        this.material.uniforms.uCorners.value,
+        {
+          z: 1,
+          duration: 1,
+        },
+        0.2
+      )
+      .to(
+        this.material.uniforms.uCorners.value,
+        {
+          w: 1,
+          duration: 1,
+        },
+        0.3
+      );
+
     this.mesh = new THREE.Mesh(this.geometry, this.material);
     this.scene.add(this.mesh);
     this.mesh.position.x = 300;
     this.mesh.rotation.z = 0.5;
-    // this.mesh.scale.set(2, 1, 1);
   }
 
   render() {
-    this.time += 0.01;
+    this.time += 0.05;
     this.material.uniforms.time.value = this.time;
-    this.material.uniforms.uProgress.value = this.settings.progress;
+    // this.material.uniforms.uProgress.value = this.settings.progress;
+    this.tl.progress(this.settings.progress);
     this.mesh.rotation.x = this.time / 2000;
     this.mesh.rotation.y = this.time / 1000;
 
